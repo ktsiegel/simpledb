672645598eb627eddfb6d5fbe22217f15cfda107
Aaron Elmore <aelmore@csail.mit.edu>
2014-09-10 14:20:15 -0400

    remove local and remote tag

--- turnInLab1.sh
+++ turnInLab1.sh
@@ -1,4 +1,6 @@
 git commit -a -m 'Lab 1'
-git tag -a lab1 -m 'Lab1'
+git tag -d lab1
+git push origin :refs/tags/lab1
+git tag -a lab1 -m 'lab1'
 git push origin master
-git push origin lab1 
+git push origin lab1
